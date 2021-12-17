9145c14216a1dc353254ecd3334930bdb80ef4e6
amagoose <amagooseoj@mail.ru>
2021-12-17 23:17:10 +0500

    some structure

--- src/main.py
+++ src/main.py
@@ -1,5 +1,23 @@
+import sys
+import argparse
+
+
 def main():
-    print('Hello, World!')
+    args = setup_and_parse(sys.argv[1:])
+
+
+def setup_and_parse(args):
+    parser = argparse.ArgumentParser()
+    #parser.add_mutually_exclusive_group()
+    parser.add_argument('-g', '--geocoder', nargs=3, type=str, required=False, metavar='address',
+                        help='Показывает координаты здания по указанному адресу')
+    result = parser.parse_args(args)
+    check_args(result)
+    return result
+
+
+def check_args(args):
+    return
 
 
 if __name__ == '__main__':
