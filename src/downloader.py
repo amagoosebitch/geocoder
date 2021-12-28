4de10df6ae24a0a6425126fdeddf415202e7e1cf
alyona <anna.poluyanenko@gmail.com>
2021-12-28 10:42:30 +0500

    check for city + try to parse xml

--- src/downloader.py
+++ src/downloader.py
@@ -1,2 +1,44 @@
-import requests
-import json
+import xml.etree.ElementTree as ET
+
+
+def is_building(elem):
+    street_flag = False
+    housenumber_flag = False
+    for child in elem:
+        if child.tag == 'tag' and 'addr:street' in child.attrib['k']:
+            street_flag = True
+        if child.tag == 'tag' and 'addr:housenumber' in child.attrib['k']:
+            housenumber_flag = True
+        if street_flag and housenumber_flag:
+            return True
+    return False
+
+
+node_tags = set()
+way_tags = set()
+tree = ET.iterparse('Пермь.xml')
+rows_count = 0
+for event, elem in tree:
+    rows_count += 1
+    if elem.tag == 'node':
+        for tag in list(elem):
+            key = tag.attrib['k'].lower()
+            if key not in ['id', 'lat', 'lon']:
+                node_tags.add(key)
+        elem.clear()
+    elif elem.tag == 'way':
+        for child in list(elem):
+            if child.tag == 'tag':
+                key = child.attrib['k'].lower()
+                if key not in ['id', 'nodes']:
+                    way_tags.add(key)
+        elem.clear()
+    elif elem.tag == 'relation':
+        is_building = is_building(list(elem))
+        for child in list(elem):
+            if child.tag == 'tag' and is_building:
+                key = child.attrib['k'].lower()
+                if key not in ['id', 'nodes']:
+                    way_tags.add(key)
+        elem.clear()
+t = 0
--- src/main.py
+++ src/main.py
@@ -1,26 +1,53 @@
 import sys
 import argparse
+import sqlite3
+import os
+import requests
+from xml.dom import minidom
 
 
 def main():
-    args = setup_and_parse(sys.argv[1:])
+    address = setup_and_parse(sys.argv[1:]).geocode
+    city_info = find_city(address)
+    if not city_info:
+        print('Такого города нет в базе данных городов России.')
+        sys.exit(-1)
+    city, region, south, west, north, east = city_info
+    street = [x for x in address
+              if x.lower().title() != city
+              and not any(char.isdigit() for char in x)][0]
+    building = [x for x in address if x != city.lower().title() and x != street][0]
+    street = street.lower().title()
+
+    # url = f"https://overpass-api.de/api/map?bbox=" \
+    #       f"{west},{south},{east},{north}"
+    # print('Делаем запрос')
+    # response = requests.get(url, stream=True)
+    # print('Ответ получен. Скачиваем файл')
+    # with open(f"{city}.xml", 'wb') as f:
+    #     for chunk in response.iter_content(chunk_size=10 * 1024 * 1024):
+    #         if chunk:
+    #             f.write(chunk)
+    # print('Загрузка окончена')
 
 
 def setup_and_parse(args):
     parser = argparse.ArgumentParser()
-    #parser.add_mutually_exclusive_group()
-    parser.add_argument('-g', '--geocoder', nargs=3, type=str, required=False, metavar='address',
-                        help='Показывает координаты здания по указанному адресу')
+    parser.add_argument('-g', '--geocode', nargs=3, type=str, required=False, metavar=('city', 'street', 'building'),
+                        help='Показывает координаты здания и полный адрес по указанному адресу')
     result = parser.parse_args(args)
-    check_args(result)
     return result
 
 
-def check_args(args):
-    return
+def find_city(values):
+    conn = sqlite3.connect(os.getcwd()[:-3] + r'db\cities.db')
+    cursor = conn.cursor()
+    for value in values:
+        cursor.execute(f'SELECT * FROM cities WHERE city="{value.lower().title()}"')
+        found_value = cursor.fetchone()
+        if found_value:
+            return found_value
 
 
 if __name__ == '__main__':
     main()
-
-
