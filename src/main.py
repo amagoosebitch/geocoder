61f58439c2f5724675f81669cfb211197c1cef0d
alyona <anna.poluyanenko@gmail.com>
2021-12-29 19:21:29 +0500

    parse xml to mongita db

--- src/main.py
+++ src/main.py
@@ -3,7 +3,9 @@ import argparse
 import sqlite3
 import os
 import requests
-from xml.dom import minidom
+import mongita
+import re
+from xml_parser import Parser
 
 
 def main():
@@ -16,20 +18,54 @@ def main():
     street = [x for x in address
               if x.lower().title() != city
               and not any(char.isdigit() for char in x)][0]
+    regex = re.compile(r"(\d+?.*?)(\w{0,1}$)")
+    for value in address:
+        if value == street:
+            continue
+        else:
+            match = re.match(regex, value)
+            if match:
+                building = value
+            else:
+                street = value
     building = [x for x in address if x != city.lower().title() and x != street][0]
     street = street.lower().title()
+    if not os.path.isdir(os.path.abspath(__file__)[:-11] + f'db-mongita\\{city}'):
+        create_city_db(city, east, west, north, south)
 
-    # КАЧАЕМ XML
-    # url = f"https://overpass-api.de/api/map?bbox=" \
-    #       f"{west},{south},{east},{north}"
-    # print('Делаем запрос')
-    # response = requests.get(url, stream=True)
-    # print('Ответ получен. Скачиваем файл')
-    # with open(f"{city}.xml", 'wb') as f:
-    #     for chunk in response.iter_content(chunk_size=10 * 1024 * 1024):
-    #         if chunk:
-    #             f.write(chunk)
-    # print('Загрузка окончена')
+
+def find_address(city, street, building):
+    to_del = ['Улица', 'Проспект', 'Бульвар', 'Аллея', 'Переулок', 'Тракт', 'Набережная']
+    if ' ' in street:
+        part1, part2 = street.split()
+        if part1 in to_del:
+            street = part2
+        else:
+            street = part1
+    path = os.path.abspath(__file__)[:-11] + f'db-mongita\\{city}'
+    client = mongita.MongitaClientDisk(host=path)
+    ways = client.db.ways
+    cursor = ways.find({'addr:housenumber': f'{building}', 'addr:street': 'Валовая улица'})
+
+
+def create_city_db(city, east, west, north, south):
+    if not os.path.isfile(os.path.abspath(__file__)[:-11] + f'xml\\{city}.xml'):
+        download_city_xml(city, east, west, north, south)
+    parser = Parser(city)
+    parser.parse()
+
+
+def download_city_xml(city, east, west, north, south):
+    url = f"https://overpass-api.de/api/map?bbox=" \
+          f"{west},{south},{east},{north}"
+    print('Делаем запрос')
+    response = requests.get(url, stream=True)
+    print('Ответ получен. Скачиваем файл')
+    with open(f"{city}.xml", 'wb') as f:
+        for chunk in response.iter_content(chunk_size=10 * 1024 * 1024):
+            if chunk:
+                f.write(chunk)
+    print('Загрузка окончена')
 
 
 def setup_and_parse(args):
---
+++ src/xml_parser.py
@@ -0,0 +1,94 @@
+import os
+from xml.etree import ElementTree
+from mongita import MongitaClientDisk
+
+NODES_COUNT = 200000
+WAYS_COUNT = 100000
+RELATIONS_COUNT = 100000
+
+
+class Parser:
+    def __init__(self, city):
+        path = os.path.abspath(__file__)[:-17] + f'db-mongita\\{city}'
+        if not os.path.exists(path):
+            os.makedirs(path)
+        client = MongitaClientDisk(host=path)
+        db = client['db']
+        self.ways = db['ways']
+        self.nodes = db['nodes']
+        self.city_file = city + '.xml'
+        self.i = 0
+        self.nodes_result = []
+        self.ways_result = []
+        self.nodes_coordinates = {}
+        self.ways_coordinates = {}
+
+    def parse(self):
+        tree = ElementTree.iterparse(os.path.abspath(__file__)[:-17] + f'xml\\{self.city_file}')
+        for _, element in tree:
+            if element.tag == 'node':
+                self.parse_node(element)
+                element.clear()
+            elif element.tag == 'way':
+                if len(self.nodes_result) > 0:
+                    self.nodes.insert_many(self.nodes_result)
+                    self.nodes_result = []
+                self.parse_way(element)
+                element.clear()
+        if len(self.ways_result) > 0:
+            self.ways.insert_many(self.ways_result)
+            self.ways_result = []
+        del tree
+
+    def parse_node(self, elem):
+        tags = list(elem)
+        attr = elem.attrib
+        result = {'id': attr['id'], 'lat': attr['lat'], 'lon': attr['lon']}
+        self.nodes_coordinates[attr['id']] = (attr['lat'], attr['lon'])
+        street = False
+        housenumber = False
+        for tag in tags:
+            key = tag.attrib['k'].lower()
+            if key == 'addr:street':
+                result[key] = tag.attrib['v']
+                street = True
+            if key == 'addr:housenumber':
+                result[key] = tag.attrib['v']
+                housenumber = True
+        self.nodes_result.append(result)
+        if len(self.nodes_result) > NODES_COUNT:
+            self.nodes.insert_many(self.nodes_result)
+            self.nodes_result = []
+        if street and housenumber:
+            self.ways.insert_many([result])
+
+    def parse_way(self, way):
+        subelements = list(way)
+        attributes = way.attrib
+        result = {'id': attributes['id']}
+        nodes = []
+        street = False
+        housenumber = False
+        for child in subelements:
+            if child.tag == 'nd':
+                ref = child.attrib['ref']
+                nodes.append(self.nodes_coordinates[ref])
+            elif child.tag == 'tag':
+                key = child.attrib['k'].lower()
+                if key == 'addr:street':
+                    street = True
+                    result[key] = child.attrib['v']
+                if key == 'addr:housenumber':
+                    housenumber = True
+                    result[key] = child.attrib['v']
+        if street and housenumber:
+            result['nodes'] = tuple(nodes)
+            self.ways_result.append(result)
+            self.ways_coordinates[attributes['id']] = tuple(nodes)
+            if len(self.ways_result) > WAYS_COUNT:
+                self.ways.insert_many(self.ways_result)
+                self.ways_result = []
+
+
+if __name__ == '__main__':
+    Parser('Челябинск').parse()
