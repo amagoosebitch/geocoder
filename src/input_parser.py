e10365c6c9e68cda4f075f50d556f0b21df164de
amagoose <amagooseoj@mail.ru>
2022-01-15 04:11:48 +0500

    afdasf

--- src/input_parser.py
+++ src/input_parser.py
@@ -179,7 +179,7 @@ class InputParser:
         return self._handle_building_deletion_info(possible_buildings[0])
 
     def _find_street(self):
-        print(self.city)
+        print(self.city, self.building)
         if len(self.dynamic_info) == 0:
             print('Неверный формат данных')
             sys.exit(-2)
