61b5d18ef6857d5a7fcc9323d2bfb416d6e19e7f
amagoose <amagooseoj@mail.ru>
2022-01-15 05:43:03 +0500

    hehe :)

--- test_file.sh
+++ test_file.sh
@@ -1,55 +1,55 @@
 #!/bin/sh
- python main.py -g 'Екатеринбург, Хохрякова 10'
- python main.py -g 'Екатеринбург, Малышева 23'
- python main.py -g 'Екатеринбург, Малышева 25'
- python main.py -g Екатеринбург, Малышева 25
- python main.py -g Екатеринбург, Малышева 25
- python main.py -g 'Екатеринбург, Малышева 25'
- python main.py -g Екатеринбург,Малышева 25
- python main.py -g Екатеринбург,Малышева,25
- python main.py -g 'Екатеринбург,Малышева,25'
- python main.py -g 'Екатеринбург,Малышева, 25'
- python main.py -g 'Екатеринбург,Малышева,,25'
- python main.py -g 'Екатеринбург,Малышева, ,25'
- python main.py -g 'Екатеринбург,Малышева,, 25'
- python main.py -g 'Екатеринбург, Малышева,,25'
- python main.py -g 'Екатеринбург ,Малышева,,25'
- python main.py -g 'Екатеринбург,Малышева,, 25'
- python main.py -g Екатеринбург,Малышева,, 25
- python main.py -g Екатеринбург 40 лет ВЛКСМ 1
- python main.py -g 'Ленина 45, Пермь'
- python main.py -g 'Ленина,45,Пермь'
- python main.py -g 'Пермь,Комсомольский пр-кт 88'
- python main.py -g 'Пермь,Комсомольский пр-кт,88'
- python main.py -g 'Пермь,Комсомольский пр-кт,86'
- python main.py -g Санкт-Петербург 18-я линия В.О., 53 лит. Б
- python main.py -g Санкт-Петербург 18-я линия В.О., 53 лит Б
- python main.py -g Санкт-Петербург 18-я линия В.О., 53 литера Б
- python main.py -g Санкт-Петербург 18-я линия В.О., 25
- python main.py -g Санкт-Петербург 18-я линия В.О., 25
- python main.py -g Санкт-Петербург, 18 линия ВО, 11
- python main.py -g Санкт-Петербург, Чехова, 4
- python main.py -g Санкт-Петербург, Чехова,4
- python main.py -g Санкт-Петербург,Чехова,4
- python main.py -g 'Санкт-Петербург, Чехова, 4'
- python main.py -g 'Санкт-Петербург, Короленко, 4'
- python main.py -g Чехова 4, Санкт-Питербург
- python main.py -g Санкт-Петербург, Руставели, 55 корпус 1 стр.2
- python main.py -g Санкт-Петербург, Руставели, 55 корп.1 строение 2
- python main.py -g Санкт-Петербург, Руставели, 55 к 1 стр. 2
- python main.py -g 'Санкт-Петербург, Сапёрный переулок, 10 лит В'
- python main.py -g г. Нижний Новогород, Ленина 1
- python main.py -g г. Нижней Новогород, Ленина 1
- python main.py -g г. Нижний Навогород, Ленина 1
- python main.py -g г. Нижний Новогород, Ленина 1
- python main.py -g г. Нижний Новгород, Ленина 1
- python main.py -g Ленин, Ленина 1
- python main.py -g 'Москвка, Льва толстого 16'
- python main.py -g 'Москва, Льва толстого 16'
- python main.py -g Москва, Льва толстого 16
- python main.py -g Москва, 'Льва толстого 16'
- python main.py -g Москва, 'Льва толстого' 16
- python main.py -g Москва, Льва Тлстого, 16
- python main.py -g Москва, Льва Тлстого, 16
- python main.py -g Москва, Льва Тлстого,16
- python main.py -g Москва, Льва Тлстого, 16
+ python3 main.py -g 'Екатеринбург, Хохрякова 10'
+ python3 main.py -g 'Екатеринбург, Малышева 23'
+ python3 main.py -g 'Екатеринбург, Малышева 25'
+ python3 main.py -g Екатеринбург, Малышева 25
+ python3 main.py -g Екатеринбург, Малышева 25
+ python3 main.py -g 'Екатеринбург, Малышева 25'
+ python3 main.py -g Екатеринбург,Малышева 25
+ python3 main.py -g Екатеринбург,Малышева,25
+ python3 main.py -g 'Екатеринбург,Малышева,25'
+ python3 main.py -g 'Екатеринбург,Малышева, 25'
+ python3 main.py -g 'Екатеринбург,Малышева,,25'
+ python3 main.py -g 'Екатеринбург,Малышева, ,25'
+ python3 main.py -g 'Екатеринбург,Малышева,, 25'
+ python3 main.py -g 'Екатеринбург, Малышева,,25'
+ python3 main.py -g 'Екатеринбург ,Малышева,,25'
+ python3 main.py -g 'Екатеринбург,Малышева,, 25'
+ python3 main.py -g Екатеринбург,Малышева,, 25
+ python3 main.py -g Екатеринбург 40 лет ВЛКСМ 1
+ python3 main.py -g 'Ленина 45, Пермь'
+ python3 main.py -g 'Ленина,45,Пермь'
+ python3 main.py -g 'Пермь,Комсомольский пр-кт 88'
+ python3 main.py -g 'Пермь,Комсомольский пр-кт,88'
+ python3 main.py -g 'Пермь,Комсомольский пр-кт,86'
+ python3 main.py -g Санкт-Петербург 18-я линия В.О., 53 лит. Б
+ python3 main.py -g Санкт-Петербург 18-я линия В.О., 53 лит Б
+ python3 main.py -g Санкт-Петербург 18-я линия В.О., 53 литера Б
+ python3 main.py -g Санкт-Петербург 18-я линия В.О., 25
+ python3 main.py -g Санкт-Петербург 18-я линия В.О., 25
+ python3 main.py -g Санкт-Петербург, 18 линия ВО, 11
+ python3 main.py -g Санкт-Петербург, Чехова, 4
+ python3 main.py -g Санкт-Петербург, Чехова,4
+ python3 main.py -g Санкт-Петербург,Чехова,4
+ python3 main.py -g 'Санкт-Петербург, Чехова, 4'
+ python3 main.py -g 'Санкт-Петербург, Короленко, 4'
+ python3 main.py -g Чехова 4, Санкт-Питербург
+ python3 main.py -g Санкт-Петербург, Руставели, 55 корпус 1 стр.2
+ python3 main.py -g Санкт-Петербург, Руставели, 55 корп.1 строение 2
+ python3 main.py -g Санкт-Петербург, Руставели, 55 к 1 стр. 2
+ python3 main.py -g 'Санкт-Петербург, Сапёрный переулок, 10 лит В'
+ python3 main.py -g г. Нижний Новогород, Ленина 1
+ python3 main.py -g г. Нижней Новогород, Ленина 1
+ python3 main.py -g г. Нижний Навогород, Ленина 1
+ python3 main.py -g г. Нижний Новогород, Ленина 1
+ python3 main.py -g г. Нижний Новгород, Ленина 1
+ python3 main.py -g Ленин, Ленина 1
+ python3 main.py -g 'Москвка, Льва толстого 16'
+ python3 main.py -g 'Москва, Льва толстого 16'
+ python3 main.py -g Москва, Льва толстого 16
+ python3 main.py -g Москва, 'Льва толстого 16'
+ python3 main.py -g Москва, 'Льва толстого' 16
+ python3 main.py -g Москва, Льва Тлстого, 16
+ python3 main.py -g Москва, Льва Тлстого, 16
+ python3 main.py -g Москва, Льва Тлстого,16
+ python3 main.py -g Москва, Льва Тлстого, 16
